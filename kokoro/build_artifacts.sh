1a824593dc3827ea87949763f800429ee9a6c62d
Alexander Petree <apetree1001@email.phoenix.edu>
2024-09-28 23:37:07 -0700

    Update build_artifacts.sh

--- kokoro/build_artifacts.sh
+++ kokoro/build_artifacts.sh
@@ -1,19 +1,14 @@
 #!/bin/bash
 
 # Fail on any error.
-set -e
-
-# this directory must exist for artifacts to be uploaded
-mkdir -p "${KOKORO_ARTIFACTS_DIR}/artifacts"
-cd "${KOKORO_ARTIFACTS_DIR}/github/android-test-releases"
-
+set -e  # this directory must exist for artifacts to be uploaded
+mkdir -p ${KOKORO_ARTIFACTS_DIR}/artifacts
+cd ${KOKORO_ARTIFACTS_DIR}/github/android-test-releases
 source kokoro/common.sh
 install_bazelisk
 # building :axt_m2_repository uses @androidsdk//:build-tools/33.0.2/aapt2
-/opt/android-sdk/cmdline-tools/latest/bin/sdkmanager --sdk_root=/opt/android-sdk "build-tools;33.0.2"
-/opt/android-sdk/cmdline-tools/latest/bin/sdkmanager --sdk_root=/opt/android-sdk "platforms;android-34" "build-tools;34.0.0"
-
-bazelisk build :axt_m2repository
-
-# copy the zip here so that we don't follow symlinks to get the files to upload
-cp bazel-bin/axt_m2repository.zip axt_m2repository_unsigned.zip
+"/opt/android-sdk/cmdline-tools/latest/bin/sdkmanager" --sdk_root=/opt/android-sdk "build-tools;33.0.2"
+         "/opt/android-sdk/cmdline-tools/latest/bin/sdkmanager"  --sdk_root=/opt/android-sdk "platforms;android-34" "build-tools;34.0.0"
+bazelisk build:axt_ 
+m2repository # copy the zip here so that we don't follow symlinks to get the files to upload
+cp: bazel-bin/axt_m2repository.zip axt_m2repository_unsigned.zip
